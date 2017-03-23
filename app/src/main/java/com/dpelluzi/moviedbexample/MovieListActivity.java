ae68b2faaf53f5d4ba95c30218c6e2eb6e1b5272
Dennis Pelluzi <dennis.pelluzi@easytaxi.com.br>
2017-03-23 16:59:05 -0300

    Add RecyclerView to show movie list

--- app/src/main/java/com/dpelluzi/moviedbexample/MovieListActivity.java
+++ app/src/main/java/com/dpelluzi/moviedbexample/MovieListActivity.java
@@ -2,22 +2,44 @@ package com.dpelluzi.moviedbexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.widget.Toast;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MovieListActivity extends AppCompatActivity {
+
+    @BindView(R.id.list)
+    RecyclerView mMovieList;
+
+    private MovieListAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+        ButterKnife.bind(this);
+
+        mMovieList.setLayoutManager(new LinearLayoutManager(this));
+        mAdapter = new MovieListAdapter();
+        mAdapter.setOnItemClickListener(new MovieListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClicked(Movie movie) {
+                Toast.makeText(MovieListActivity.this, movie.title, Toast.LENGTH_SHORT).show();
+            }
+        });
+        mMovieList.setAdapter(mAdapter);
 
         MovieRepository.getInstance().getNowPlayingMovies(new MovieRepository.GetMoviesCallback() {
             @Override
             public void onSuccess(List<Movie> movies) {
                 Log.d("TMDB", "sucess");
+                mAdapter.addItems(movies);
             }
 
             @Override
---
+++ app/src/main/java/com/dpelluzi/moviedbexample/MovieListAdapter.java
@@ -0,0 +1,88 @@
+package com.dpelluzi.moviedbexample;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.OnItemClick;
+import butterknife.OnItemLongClick;
+
+public class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.MovieViewHolder> {
+
+    public interface OnItemClickListener {
+        void onItemClicked(Movie movie);
+    }
+
+    private List<Movie> mData;
+    private OnItemClickListener mOnItemClickListener;
+
+    public MovieListAdapter() {
+        mData = new ArrayList<>();
+    }
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_movie_list, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieViewHolder holder, int position) {
+        holder.bind(mData.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void addItems(List<Movie> items) {
+        mData.addAll(items);
+        notifyDataSetChanged();
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mOnItemClickListener = listener;
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder {
+
+        @BindView(R.id.text_title)
+        TextView titleView;
+
+        @BindView(R.id.text_overview)
+        TextView overviewTextView;
+
+        @BindView(R.id.text_rating)
+        TextView ratingView;
+
+        public MovieViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+
+        public void bind(@NonNull Movie item) {
+            titleView.setText(item.title);
+            overviewTextView.setText(item.overview);
+            ratingView.setText(ratingView.getResources().getString(R.string.text_rating, item.voteAverage));
+        }
+
+        @OnClick(R.id.list_item)
+        public void onItemClicked() {
+            if (mOnItemClickListener != null) {
+                mOnItemClickListener.onItemClicked(mData.get(getAdapterPosition()));
+            }
+        }
+    }
+}
